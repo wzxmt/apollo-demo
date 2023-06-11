3c413a7658dcd8a63a0a574eee0a0e7a0c61f0e1
Administrator <wzxmt@163.com>
2023-06-12 00:27:24 +0800

    update config

--- src/main/java/wzxmt/apollodemo/controller/UsaerController.java
+++ src/main/java/wzxmt/apollodemo/controller/UsaerController.java
@@ -1,14 +1,22 @@
 package wzxmt.apollodemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsaerController {
-    @Value("${hello}")
+    @Value("${hello:hello}")
     private String hello;
-    @RequestMapping ("/boot/hello")
-    public String hello(){
+    @RequestMapping ("/hello")
+    public String hello() {
         return hello;
+    }
+    @Value("${test:默认值}")
+    private String test;
+    @GetMapping("/test")
+    public String test(){
+        return "test的值为: " + test;
     }
 }
