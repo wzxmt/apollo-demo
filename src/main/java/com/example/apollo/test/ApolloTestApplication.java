e43ea22ce0b939a094257b4236534c4545d7ea42
Administrator <wzxmt@163.com>
2023-06-12 12:28:05 +0800

    update config

--- src/main/java/com/example/apollo/test/ApolloTestApplication.java
+++ src/main/java/com/example/apollo/test/ApolloTestApplication.java
@@ -1,21 +1,34 @@
 package com.example.apollo.test;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class ApolloTestApplication {
-    @Value("${hello:hello}")
-    private String hello;
-    @RequestMapping ("/hello")
-    public String hello() {
-        return hello;
-    }
-    @Value("${test:默认值}")
-    private String test;
-    @GetMapping("/test")
-    public String test(){
-        return "test的值为: " + test;
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloTestApplication.class, args);
+	}
+
+	@GetMapping("/hi")
+	public String hi() {
+		return "hi, devops";
+	}
+	@Value("${hello:hello}")
+	private String hello;
+	@RequestMapping ("/hello")
+	public String hello() {
+		return hello;
+	}
+	@Value("${test:默认值}")
+	private String test;
+	@GetMapping("/test")
+	public String test(){
+		return "test的值为: " + test;
+	}
 }
